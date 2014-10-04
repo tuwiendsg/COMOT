6f7d9a64972193866e5d1beceb617cddfe834dd2
Daniel Moldovan <d.moldovan@dsg.tuwien.ac.at>
2014-10-04 13:47:39 +0200

    Added Visualization service

---
+++ comot-orchestrator/src/test/java/at/ac/tuwien/dsg/comot/orchestrator/ProgrammingAndControllingElasticityWithCOMOT_GXG.java
@@ -0,0 +1,182 @@
+package at.ac.tuwien.dsg.comot.orchestrator;
+
+import static at.ac.tuwien.dsg.comot.common.model.ArtifactTemplate.SingleScriptArtifactTemplate;
+import at.ac.tuwien.dsg.comot.common.model.Capability;
+import static at.ac.tuwien.dsg.comot.common.model.CommonOperatingSystemSpecification.OpenstackMicro;
+import static at.ac.tuwien.dsg.comot.common.model.CommonOperatingSystemSpecification.OpenstackSmall;
+import at.ac.tuwien.dsg.comot.common.model.Constraint;
+import at.ac.tuwien.dsg.comot.common.model.Constraint.Metric;
+import static at.ac.tuwien.dsg.comot.common.model.EntityRelationship.ConnectToRelation;
+import static at.ac.tuwien.dsg.comot.common.model.EntityRelationship.HostedOnRelation;
+import at.ac.tuwien.dsg.comot.common.model.OperatingSystemUnit;
+import static at.ac.tuwien.dsg.comot.common.model.OperatingSystemUnit.OperatingSystemUnit;
+import at.ac.tuwien.dsg.comot.common.model.Requirement;
+import at.ac.tuwien.dsg.comot.common.model.ServiceTemplate;
+import static at.ac.tuwien.dsg.comot.common.model.ServiceTemplate.ServiceTemplate;
+import at.ac.tuwien.dsg.comot.common.model.ServiceTopology;
+import static at.ac.tuwien.dsg.comot.common.model.ServiceTopology.ServiceTopology;
+import at.ac.tuwien.dsg.comot.common.model.ServiceUnit;
+import static at.ac.tuwien.dsg.comot.common.model.SoftwareNode.SingleSoftwareUnit;
+import at.ac.tuwien.dsg.comot.common.model.Strategy;
+import static at.ac.tuwien.dsg.comot.common.model.Strategy.Strategy;
+import at.ac.tuwien.dsg.orchestrator.interraction.COMOTOrchestrator;
+
+/**
+ *
+ * @author http://dsg.tuwien.ac.at
+ */
+public class ProgrammingAndControllingElasticityWithCOMOT {
+
+    public static void main(String[] args) {
+        //specify service units in terms of software
+
+        //need to specify details of VM and operating system to deploy the software servide units on
+        OperatingSystemUnit dataControllerVM = OperatingSystemUnit("DataControllerVM")
+                .providedBy(OpenstackSmall("OpenStackSmall_OS_DC")
+                        .withProvider("dsg@openstack")
+                        .addSoftwarePackage("openjdk-7-jre")
+                        .addSoftwarePackage("ganglia-monitor")
+                        .addSoftwarePackage("gmetad")
+                );
+
+        OperatingSystemUnit dataNodeVM = OperatingSystemUnit("DataNodeVM")
+                .providedBy(OpenstackMicro("OpenStackMicro_OS_DN")
+                        .withProvider("dsg@openstack")
+                        .addSoftwarePackage("openjdk-7-jre")
+                        .addSoftwarePackage("ganglia-monitor")
+                        .addSoftwarePackage("gmetad")
+                );
+
+        //finally, we define Vm types for event processing
+        OperatingSystemUnit loadbalancerVM = OperatingSystemUnit("LoadbalancerVM")
+                .providedBy(OpenstackSmall("OpenStackSmall_OS_LB")
+                        .withProvider("dsg@openstack")
+                        .addSoftwarePackage("openjdk-7-jre")
+                        .addSoftwarePackage("ganglia-monitor")
+                        .addSoftwarePackage("gmetad")
+                );
+
+        OperatingSystemUnit eventProcessingVM = OperatingSystemUnit("EventProcessingVM")
+                .providedBy(OpenstackMicro("OpenStackMicro_OS_EP")
+                        .withProvider("dsg@openstack")
+                        .addSoftwarePackage("openjdk-7-jre")
+                        .addSoftwarePackage("ganglia-monitor")
+                        .addSoftwarePackage("gmetad")
+                );
+
+        //start with Data End, and first with Data Controller
+        ServiceUnit dataControllerUnit = SingleSoftwareUnit("DataControllerUnit")
+                //software artifacts needed for unit deployment   = script to deploy Cassandra
+                .deployedBy(SingleScriptArtifactTemplate("deployDataControllerArtifact", "deployCassandraSeed.sh"))
+                //data controller exposed its IP 
+                .exposes(Capability.Variable("DataController_IP_information"));
+
+        //specify data node
+        ServiceUnit dataNodeUnit = SingleSoftwareUnit("DataNodeUnit")
+                .deployedBy(SingleScriptArtifactTemplate("deployDataNodeArtifact", "deployCassandraNode.sh"))
+                //data node MUST KNOW the IP of cassandra seed, to connect to it and join data cluster
+                .requires(Requirement.Variable("DataController_IP_Data_Node_Req").withName("requiringDataNodeIP"))
+                //express elasticity strategy: Scale IN Data Node when cpu usage < 40%
+                .controlledBy(Strategy("ST1")
+                        .when(Constraint.MetricConstraint("ST1CO1", new Metric("cpuUsage", "%")).lessThan("40"))
+                        .then(Strategy.Action.ScaleIn)
+                );
+
+        //add the service units belonging to the event processing topology
+        ServiceUnit eventProcessingUnit = SingleSoftwareUnit("EventProcessingUnit")
+                .deployedBy(SingleScriptArtifactTemplate("deployEventProcessingArtifact", "deployEventProcessing.sh"))
+                //event processing must register in Load Balancer, so it needs the IP
+                .requires(Requirement.Variable("EventProcessingUnit_LoadBalancer_IP_Req"))
+                //event processing also needs to querry the Data Controller to access data
+                .requires(Requirement.Variable("EventProcessingUnit_DataController_IP_Req"))
+                //scale IN if throughput < 200 and responseTime < 200
+                .controlledBy(Strategy("ST2")
+                        .when(Constraint.MetricConstraint("ST2CO1", new Metric("responseTime", "ms")).lessThan("200"))
+                        .and(Constraint.MetricConstraint("ST2CO2", new Metric("throughput", "operations/s")).lessThan("200"))
+                        .then(Strategy.Action.ScaleIn)
+                );
+
+        //add the service units belonging to the event processing topology
+        ServiceUnit loadbalancerUnit = SingleSoftwareUnit("LoadBalancerUnit")
+                //load balancer must provide IP
+                .exposes(Capability.Variable("LoadBalancer_IP_information"))
+                .deployedBy(SingleScriptArtifactTemplate("deployLoadBalancerArtifact", "deployLoadBalancer.sh"));
+
+        //Describe a Data End service topology containing the previous 2 software service units
+        ServiceTopology dataEndTopology = ServiceTopology("DataEndTopology")
+                .withServiceUnits(dataControllerUnit, dataNodeUnit //add also OS units to topology
+                        , dataControllerVM, dataNodeVM
+                );
+
+        //specify constraints on the data topology
+        //thus, the CPU usage of all Service Unit instances of the data end Topology must be below 80%
+        dataEndTopology.constrainedBy(Constraint.MetricConstraint("DataEndCO1", new Metric("cpuUsage", "%")).lessThan("40"));
+
+        //define event processing unit topology
+        ServiceTopology eventProcessingTopology = ServiceTopology("EventProcessingTopology")
+                .withServiceUnits(loadbalancerUnit, eventProcessingUnit //add vm types to topology
+                        , loadbalancerVM, eventProcessingVM
+                );
+        
+        eventProcessingTopology.constrainedBy(Constraint.MetricConstraint("C02", new Metric("responseTime", "ms")).lessThan("400"));
+
+        //describe the service template which will hold more topologies
+        ServiceTemplate serviceTemplate = ServiceTemplate("DaasService")
+                .consistsOfTopologies(dataEndTopology)
+                .consistsOfTopologies(eventProcessingTopology)
+                //defining CONNECT_TO and HOSTED_ON relationships
+                .andRelationships(
+                        //Data Controller IP send to Data Node
+                        ConnectToRelation("dataNodeToDataController")
+                        .from(dataControllerUnit.getContext().get("DataController_IP_information"))
+                        .to(dataNodeUnit.getContext().get("DataController_IP_Data_Node_Req")) //specify which software unit goes to which VM
+                        ,
+                        //event processing gets IP from load balancer
+                        ConnectToRelation("eventProcessingToLoadBalancer")
+                        .from(loadbalancerUnit.getContext().get("LoadBalancer_IP_information"))
+                        .to(eventProcessingUnit.getContext().get("EventProcessingUnit_LoadBalancer_IP_Req")) //specify which software unit goes to which VM
+                        ,
+                        //event processing gets IP from data controller
+                        ConnectToRelation("eventProcessingToDataController")
+                        .from(dataControllerUnit.getContext().get("DataController_IP_information"))
+                        .to(eventProcessingUnit.getContext().get("EventProcessingUnit_DataController_IP_Req")) //specify which software unit goes to which VM
+                        ,
+                        HostedOnRelation("dataControllerToVM")
+                        .from(dataControllerUnit)
+                        .to(dataControllerVM),
+                        HostedOnRelation("dataNodeToVM")
+                        .from(dataNodeUnit)
+                        .to(dataNodeVM) //add hosted on relatinos
+                        , HostedOnRelation("loadbalancerToVM")
+                        .from(loadbalancerUnit)
+                        .to(loadbalancerVM), HostedOnRelation("eventProcessingToVM")
+                        .from(eventProcessingUnit)
+                        .to(eventProcessingVM)
+                )
+                // as we have horizontally scalable distributed systems (one service unit can have more instances)
+                //metrics must be aggregated among VMs
+                .withDefaultMetrics()
+                //to find scaling actions, one must assume some effects for each action, to understand
+                //if it makes sense or not to execute the action
+                .withDefaultActionEffects()
+                
+                ;
+        
+                //instantiate COMOT orchestrator to deploy, monitor and control the service
+                COMOTOrchestrator orchestrator = new COMOTOrchestrator()
+                        //we have SALSA as cloud management tool
+                        //curently deployed separately
+                        .withSalsaIP("128.130.172.215")
+                        .withSalsaPort(8080)
+                        
+                        //we have rSYBL elasticity control service and MELA 
+                        //deployed separately
+                        .withRsyblIP("128.130.172.214")
+                        .withRsyblPort(8081);
+                
+                //deploy, monitor and control
+                orchestrator.deployAndControl(serviceTemplate);
+//                orchestrator.controlExisting(serviceTemplate);
+                
+    }
+}
