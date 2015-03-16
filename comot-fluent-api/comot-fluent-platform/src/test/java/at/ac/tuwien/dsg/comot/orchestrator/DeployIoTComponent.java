3dae9a2716836b36e66dc4e6b416868406f0f8e7
Daniel Moldovan <d.moldovan@dsg.tuwien.ac.at>
2015-03-16 15:12:31 +0100

    Updated deploying IoT sensors to have only maximum one gateway and one sensor per VM, until we test things completely.

--- comot-fluent-api/comot-fluent-platform/src/test/java/at/ac/tuwien/dsg/comot/orchestrator/DeployIoTComponent.java
+++ comot-fluent-api/comot-fluent-platform/src/test/java/at/ac/tuwien/dsg/comot/orchestrator/DeployIoTComponent.java
@@ -37,78 +37,75 @@ import at.ac.tuwien.dsg.comot.orchestrator.interraction.COMOTOrchestrator;
  * @author hungld
  */
 public class DeployIoTComponent {
-     public static void main(String[] args) {
-         
-         String salsaRepo = "http://128.130.172.215/salsa/upload/files/rtGovOps/";
-         
-         ServiceUnit MqttQueueVM = OperatingSystemUnit("MqttQueueVM")
-                 .providedBy(OpenstackSmall())                 
-                 .andReference("ElasticIoTPlatform/MqttQueueVM");
-         
-         ServiceUnit QueueUnit = SoftwareNode.SingleSoftwareUnit("QueueUnit")
-                 .exposes(Capability.Variable("brokerIp_Capability"))
-                 .andReference("ElasticIoTPlatform/QueueUnit");
-         
-         
-         
-         OperatingSystemUnit gatewayVM = OperatingSystemUnit("gatewayVM")
+
+    public static void main(String[] args) {
+
+        String salsaRepo = "http://128.130.172.215/salsa/upload/files/rtGovOps/";
+
+        ServiceUnit MqttQueueVM = OperatingSystemUnit("MqttQueueVM")
+                .providedBy(OpenstackSmall())
+                .andReference("ElasticIoTPlatform/MqttQueueVM");
+
+        ServiceUnit QueueUnit = SoftwareNode.SingleSoftwareUnit("QueueUnit")
+                .exposes(Capability.Variable("brokerIp_Capability"))
+                .andReference("ElasticIoTPlatform/QueueUnit");
+
+        OperatingSystemUnit gatewayVM = OperatingSystemUnit("gatewayVM")
                 .providedBy(OpenstackSmall()
-                        .withBaseImage("7ac2cc53-2301-40d7-a030-910d72f552ff")  // this image include docker, faster spin up
-//                        .addSoftwarePackage("ganglia-monitor")
-//                        .addSoftwarePackage("gmetad")
-                );
-                  
-         DockerUnit gatewayDocker = DockerUnit("gatewayDocker")
-                 .providedBy(DockerDefault())
+                        .withBaseImage("7ac2cc53-2301-40d7-a030-910d72f552ff") // this image include docker, faster spin up
+                //                        .addSoftwarePackage("ganglia-monitor")
+                //                        .addSoftwarePackage("gmetad")
+                ).withMaxColocatedInstances(1);
+
+        DockerUnit gatewayDocker = DockerUnit("gatewayDocker")
+                .providedBy(DockerDefault())
                 .deployedBy(DockerFileArtifact("dockerFileArtifact", salsaRepo + "Dockerfile-UB"),
-                            MiscArtifact("starter.sh", salsaRepo + "starter_ubuntu.sh"),
-                            MiscArtifact("achieveArtifact", salsaRepo + "rtGovOps-agents.tar.gz"));
-         
-          ServiceUnit sensorUnit = SingleSoftwareUnit("sensorUnit")
-                  .requires(Requirement.Variable("brokerIp_Requirement"))
-                .deployedBy(SingleScriptArtifact("deploySensorUnit", salsaRepo + "deploySensorUnit.sh"));
+                        MiscArtifact("starter.sh", salsaRepo + "starter_ubuntu.sh"),
+                        MiscArtifact("achieveArtifact", salsaRepo + "rtGovOps-agents.tar.gz"))
+                .withMaxColocatedInstances(1);
+
+        ServiceUnit sensorUnit = SingleSoftwareUnit("sensorUnit")
+                .requires(Requirement.Variable("brokerIp_Requirement"))
+                .deployedBy(SingleScriptArtifact("deploySensorUnit", salsaRepo + "deploySensorUnit.sh"))
+                .withMaxColocatedInstances(1);
 //                , MiscArtifact("sensor.tar.gz", salsaRepo + "sensor.tar.gz"));
-         
-         
-         ServiceTopology gatewayTopology = ServiceTopology("IoTTopology")
+
+        ServiceTopology gatewayTopology = ServiceTopology("IoTTopology")
                 .withServiceUnits(sensorUnit, gatewayVM, gatewayDocker)
                 .withServiceUnits(MqttQueueVM, QueueUnit);
-         
-         CloudService serviceTemplate = ServiceTemplate("IoTSensors")
-//         CloudService serviceTemplate = ServiceTemplate("GatewayDockerService")
+
+        CloudService serviceTemplate = ServiceTemplate("IoTSensors")
+                //         CloudService serviceTemplate = ServiceTemplate("GatewayDockerService")
                 .consistsOfTopologies(gatewayTopology)
                 //defining CONNECT_TO and HOSTED_ON relationships
                 .andRelationships(HostedOnRelation("dockerOnVM")
                         .from(gatewayDocker)
                         .to(gatewayVM))
-                 .andRelationships(HostedOnRelation("SensorOnDocker")
-                         .from(sensorUnit)
-                         .to(gatewayDocker))
-                 .andRelationships(HostedOnRelation("QueueUnitOnMqttQueueVM")
-                         .from(QueueUnit)
-                         .to(MqttQueueVM))
-                 // note: the ID of connectto relationship for Sensors must be started with "mqtt", the sensor code is hard-coded to read this pattern.
-                 .andRelationships(ConnectToRelation("mqtt-broker")
-                         .from(QueueUnit.getContext().get("brokerIp_Capability"))
-                         .to(sensorUnit.getContext().get("brokerIp_Requirement")));
-         
-         ToscaDescriptionBuilder toscaBuilder= new ToscaDescriptionBuilderImpl();
-         String tosca = toscaBuilder.toXml(serviceTemplate);
-         System.out.println(tosca);
-         
-         COMOTOrchestrator orchestrator = new COMOTOrchestrator()
+                .andRelationships(HostedOnRelation("SensorOnDocker")
+                        .from(sensorUnit)
+                        .to(gatewayDocker))
+                .andRelationships(HostedOnRelation("QueueUnitOnMqttQueueVM")
+                        .from(QueueUnit)
+                        .to(MqttQueueVM))
+                // note: the ID of connectto relationship for Sensors must be started with "mqtt", the sensor code is hard-coded to read this pattern.
+                .andRelationships(ConnectToRelation("mqtt-broker")
+                        .from(QueueUnit.getContext().get("brokerIp_Capability"))
+                        .to(sensorUnit.getContext().get("brokerIp_Requirement")));
+
+        ToscaDescriptionBuilder toscaBuilder = new ToscaDescriptionBuilderImpl();
+        String tosca = toscaBuilder.toXml(serviceTemplate);
+        System.out.println(tosca);
+
+        COMOTOrchestrator orchestrator = new COMOTOrchestrator()
                 //we have SALSA as cloud management tool
                 //curently deployed separately
                 .withSalsaIP("128.130.172.215")
                 .withSalsaPort(8380)
- 
                 //ifwe have rSYBL elasticity control service and MELA 
                 //deployed separately
                 .withRsyblIP("128.130.172.215")
                 .withRsyblPort(8280);
- 
+
         orchestrator.deploy(serviceTemplate);
-     }
+    }
 }
-
-
