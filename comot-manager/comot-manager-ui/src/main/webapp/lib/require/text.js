ae1b8de36cf436ab92974d1190de81502f0fa7b4
Juraj <jurajcik@gmail.com>
2015-04-29 14:15:01 +0200

    added copyright

--- comot-manager/comot-manager-ui/src/main/webapp/lib/require/text.js
+++ comot-manager/comot-manager-ui/src/main/webapp/lib/require/text.js
@@ -1,9 +1,4 @@
-﻿/**
- * @license RequireJS text 2.0.7 Copyright (c) 2010-2012, The Dojo Foundation All Rights Reserved.
- * Available via the MIT or new BSD license.
- * see: http://github.com/requirejs/text for details
- */
-/*jslint regexp: true */
+﻿/*jslint regexp: true */
 /*global require, XMLHttpRequest, ActiveXObject,
   define, window, process, Packages,
   java, location, Components, FileUtils */
