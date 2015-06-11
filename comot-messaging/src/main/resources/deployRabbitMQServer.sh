b63c32a1b6db3e19fb142241245f588368be7c23
Svetoslav Videnov <svetoslav.videnov@infosys.tuwien.ac.at>
2015-06-11 15:04:34 +0200

    rabbitMQ server setup script

--- comot-messaging/src/main/resources/deployRabbitMQServer.sh
+++ comot-messaging/src/main/resources/deployRabbitMQServer.sh
@@ -1,3 +1,22 @@
-echo "Starting server deployment!" > /tmp/salsa.rabbitMQServer.txt
+workingDir=/tmp
+logFile=$workingDir/deploayRabbitMQServer.log
 
-echo "Finished server deployment!" > /tmp/salsa.rabbitMQServer.txt
+mkdir $workingDir
+
+sudo apt-get -y install erlang
+
+wget --directory-prefix=$workingDir https://github.com/rabbitmq/rabbitmq-server/releases/download/rabbitmq_v3_5_3/rabbitmq-server-generic-unix-3.5.3.tar.gz
+
+if [ $? -ne 0 ]; then
+	echo "Could not download rabbitMQ tar file!" >> $logFile
+	exit
+fi
+
+tar -xf $workingDir/rabbitmq-server-generic-unix-3.5.3.tar.gz --directory=$workingDir
+
+if [ $? -ne 0 ]; then
+	echo "Could not extract rabbitMQ!" >> $logFile
+	exit
+fi
+
+$workingDir/rabbitmq_server-3.5.3/sbin/rabbitmq-server -detached >> $logFile
