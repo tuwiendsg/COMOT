d77b972b752ce21e0849cf1a1ba89f1346621915
Svetoslav Videnov <svetoslav.videnov@infosys.tuwien.ac.at>
2015-06-21 17:41:19 +0200

    powerDns orchestrator

---
+++ comot-fluent-api/comot-fluent-platform/src/test/java/at/ac/tuwien/dsg/comot/orchestrator/PowerDNSServer.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package at.ac.tuwien.dsg.comot.orchestrator;
+
+import at.ac.tuwien.dsg.comot.common.model.ArtifactTemplate;
+import at.ac.tuwien.dsg.comot.common.model.CloudService;
+import static at.ac.tuwien.dsg.comot.common.model.CommonOperatingSystemSpecification.OpenstackSmall;
+import at.ac.tuwien.dsg.comot.common.model.EntityRelationship;
+import static at.ac.tuwien.dsg.comot.common.model.OperatingSystemUnit.OperatingSystemUnit;
+import at.ac.tuwien.dsg.comot.common.model.ServiceTopology;
+import at.ac.tuwien.dsg.comot.common.model.ServiceUnit;
+import at.ac.tuwien.dsg.comot.common.model.SoftwareNode;
+import at.ac.tuwien.dsg.comot.common.model.OperatingSystemUnit;
+import at.ac.tuwien.dsg.comot.orchestrator.interraction.COMOTOrchestrator;
+
+/**
+ *
+ * @author vauvenal5
+ */
+public class PowerDNSServer {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		String salsaRepo = "http://128.130.172.215/salsa/upload/files/TMP/comot-messaging/";
+
+        //need to specify details of VM and operating system to deploy the software servide units on
+        OperatingSystemUnit powerDnsServerVM = OperatingSystemUnit("PowerDnsVM")
+                .providedBy(OpenstackSmall()
+                        .withBaseImage("a82e054f-4f01-49f9-bc4c-77a98045739c")
+                );
+		
+		ServiceUnit powerDnsServerUnit = SoftwareNode.SingleSoftwareUnit("PowerDnsUnit")
+				.deployedBy(ArtifactTemplate.SingleScriptArtifact("deployPowerDnsServerArtifact", salsaRepo+"deployPowerDnsServer.sh"));
+		
+		ServiceTopology powerDnsTopology = ServiceTopology.ServiceTopology("PowerDnsServerTopology")
+				.withServiceUnits(powerDnsServerUnit, powerDnsServerVM);
+		
+		CloudService powerDnsService = CloudService.ServiceTemplate("PowerDnsServerService")
+				.consistsOfTopologies(powerDnsTopology)
+				.andRelationships(
+						EntityRelationship.HostedOnRelation("powerDnsServerToVM")
+						.from(powerDnsServerUnit)
+						.to(powerDnsServerVM)
+				)
+				.withDefaultMetrics();
+		
+		COMOTOrchestrator orchestrator = new COMOTOrchestrator()
+				.withIP("128.130.172.215")
+                .withSalsaPort(8380);
+		
+		orchestrator.deploy(powerDnsService);
+	}
+	
+}
